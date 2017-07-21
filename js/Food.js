6185dd9fd0077868f30a223c530b0348a81191a3
OneGoose <xjz93@outlook.com>
2017-07-21 19:30:25 +0800

    change somthing

--- js/Food.js
+++ js/Food.js
@@ -7,7 +7,7 @@ function Food(){
 		this.x = parseInt(Math.random() * 15);
 		this.y = parseInt(Math.random() * 15);
 		//检测在不在蛇的身上
-		//我们现在要遍历蛇的身体，一个一个检测产生的食物是不是蛇的某一个身体位置
+		//现在要遍历蛇的身体，一个一个检测产生的食物是不是蛇的某一个身体位置
 		for(var i = 0 ; i < g.snake.body.length ; i++){
 			if(g.snake.body[i].x == this.x && g.snake.body[i].y == this.y){
 				break;
@@ -19,6 +19,5 @@ function Food(){
 		}
 	}
 		
-	// // alert("我是food，我被new了");
-	// g.tds[this.x][this.y].innerHTML = "*";
+
 }
